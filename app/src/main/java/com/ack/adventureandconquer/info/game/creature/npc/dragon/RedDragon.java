36d9e0f1cad34c7b86c6820241048393d066bb4b
Sascha <sascha.haeusler@netzbarkeit.ch>
2015-01-18 13:05:22 +0100

    Added dragon

---
+++ app/src/main/java/com/ack/adventureandconquer/info/game/creature/npc/dragon/RedDragon.java
@@ -0,0 +1,9 @@
+package com.ack.adventureandconquer.info.game.creature.npc.dragon;
+
+import com.ack.adventureandconquer.info.game.creature.npc.IsNpc;
+
+/**
+ * Created by saskyrar on 18/01/15.
+ */
+public class RedDragon implements IsNpc {
+}
