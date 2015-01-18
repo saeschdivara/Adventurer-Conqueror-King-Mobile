eff7889e93f712274c9178b1b8c61d3d5182396b
Sascha <sascha.haeusler@netzbarkeit.ch>
2015-01-18 11:38:57 +0100

    Added swimmers

--- app/src/main/java/com/ack/adventureandconquer/info/game/creature/SwimmerType.java
+++ app/src/main/java/com/ack/adventureandconquer/info/game/creature/SwimmerType.java
@@ -1,7 +1,32 @@
 package com.ack.adventureandconquer.info.game.creature;
 
 import com.ack.adventureandconquer.info.game.adventure.IsTerrain;
+import com.ack.adventureandconquer.info.game.adventure.Ocean;
+import com.ack.adventureandconquer.info.game.adventure.River;
+import com.ack.adventureandconquer.info.game.adventure.Swamp;
 import com.ack.adventureandconquer.info.game.creature.npc.IsNpc;
+import com.ack.adventureandconquer.info.game.creature.npc.animal.CrocodileType;
+import com.ack.adventureandconquer.info.game.creature.npc.animal.GiantCrabType;
+import com.ack.adventureandconquer.info.game.creature.npc.animal.GiantLeechType;
+import com.ack.adventureandconquer.info.game.creature.npc.humanoid.LizardManType;
+import com.ack.adventureandconquer.info.game.creature.npc.humanoid.NaiadType;
+import com.ack.adventureandconquer.info.game.creature.npc.swimmer.CatfishType;
+import com.ack.adventureandconquer.info.game.creature.npc.swimmer.DragonTurtleType;
+import com.ack.adventureandconquer.info.game.creature.npc.swimmer.GiantCrocodile;
+import com.ack.adventureandconquer.info.game.creature.npc.swimmer.GiantOctopusType;
+import com.ack.adventureandconquer.info.game.creature.npc.swimmer.GiantSquidType;
+import com.ack.adventureandconquer.info.game.creature.npc.swimmer.InsectSwarmType;
+import com.ack.adventureandconquer.info.game.creature.npc.swimmer.LargeCrocodile;
+import com.ack.adventureandconquer.info.game.creature.npc.swimmer.MermanType;
+import com.ack.adventureandconquer.info.game.creature.npc.swimmer.PiranhaFishType;
+import com.ack.adventureandconquer.info.game.creature.npc.swimmer.SeaDragonType;
+import com.ack.adventureandconquer.info.game.creature.npc.swimmer.SeaHydraType;
+import com.ack.adventureandconquer.info.game.creature.npc.swimmer.SeaSerpentType;
+import com.ack.adventureandconquer.info.game.creature.npc.swimmer.SeaSnakeType;
+import com.ack.adventureandconquer.info.game.creature.npc.swimmer.SharkType;
+import com.ack.adventureandconquer.info.game.creature.npc.swimmer.SkitteringMawType;
+import com.ack.adventureandconquer.info.game.creature.npc.swimmer.SturgeonFishType;
+import com.ack.adventureandconquer.info.game.creature.npc.swimmer.WhaleType;
 
 /**
  * Created by saskyrar on 15/01/15.
@@ -9,6 +34,129 @@ import com.ack.adventureandconquer.info.game.creature.npc.IsNpc;
 public class SwimmerType implements IsCreatureType {
     @Override
     public IsNpc getNpcType(IsTerrain terrain, int number) {
-        return null;
+        IsNpc npc = null;
+
+        if (terrain instanceof River) {
+            switch (number) {
+                case 1:
+                    npc = new GiantCrabType();
+                    break;
+                case 2:
+                    npc = new CrocodileType();
+                    break;
+                case 3:
+                    npc = new CrocodileType();
+                    break;
+                case 4:
+                    npc = new LargeCrocodile();
+                    break;
+                case 5:
+                    npc = new CatfishType();
+                    break;
+                case 6:
+                    npc = new PiranhaFishType();
+                    break;
+                case 7:
+                    npc = new SturgeonFishType();
+                    break;
+                case 8:
+                    npc = new GiantLeechType();
+                    break;
+                case 9:
+                    npc = new LizardManType();
+                    break;
+                case 10:
+                    npc = new MermanType();
+                    break;
+                case 11:
+                    npc = new NaiadType();
+                    break;
+                case 12:
+                    npc = new SkitteringMawType();
+                    break;
+            }
+        }
+        else if (terrain instanceof Ocean) {
+            switch (number) {
+                case 1:
+                    npc = new DragonTurtleType();
+                    break;
+                case 2:
+                    npc = new SeaHydraType();
+                    break;
+                case 3:
+                    npc = new MermanType();
+                    break;
+                case 4:
+                    npc = new GiantOctopusType();
+                    break;
+                case 5:
+                    npc = new SeaDragonType();
+                    break;
+                case 6:
+                    npc = new SeaSerpentType();
+                    break;
+                case 7:
+                    npc = new SharkType();
+                    break;
+                case 8:
+                    npc = new SharkType();
+                    break;
+                case 9:
+                    npc = new SkitteringMawType();
+                    break;
+                case 10:
+                    npc = new SeaSnakeType();
+                    break;
+                case 11:
+                    npc = new GiantSquidType();
+                    break;
+                case 12:
+                    npc = new WhaleType();
+                    break;
+            }
+        }
+        else if (terrain instanceof Swamp) {
+            switch (number) {
+                case 1:
+                    npc = new GiantCrabType();
+                    break;
+                case 2:
+                    npc = new CrocodileType();
+                    break;
+                case 3:
+                    npc = new LargeCrocodile();
+                    break;
+                case 4:
+                    npc = new GiantCrocodile();
+                    break;
+                case 5:
+                    npc = new CatfishType();
+                    break;
+                case 6:
+                    npc = new InsectSwarmType();
+                    break;
+                case 7:
+                    npc = new InsectSwarmType();
+                    break;
+                case 8:
+                    npc = new GiantLeechType();
+                    break;
+                case 9:
+                    npc = new GiantLeechType();
+                    break;
+                case 10:
+                    npc = new LizardManType();
+                    break;
+                case 11:
+                    npc = new LizardManType();
+                    break;
+                case 12:
+                    npc = new SkitteringMawType();
+                    break;
+            }
+        }
+
+        return npc;
     }
 }
---
+++ app/src/main/java/com/ack/adventureandconquer/info/game/creature/npc/swimmer/SharkType.java
@@ -0,0 +1,9 @@
+package com.ack.adventureandconquer.info.game.creature.npc.swimmer;
+
+import com.ack.adventureandconquer.info.game.creature.npc.IsNpc;
+
+/**
+ * Created by saskyrar on 18/01/15.
+ */
+public class SharkType implements IsNpc {
+}
