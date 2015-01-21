d78b4241b5a2368eca87c4ef2cdf2dae7a5f7bd2
Sascha <sascha.haeusler@netzbarkeit.ch>
2015-01-21 16:44:40 +0100

    Now we have a first list of monsters found

--- app/src/main/java/com/ack/adventureandconquer/EncounterDetailActivity.java
+++ app/src/main/java/com/ack/adventureandconquer/EncounterDetailActivity.java
@@ -1,5 +1,6 @@
 package com.ack.adventureandconquer;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
@@ -8,9 +9,15 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.ack.adventureandconquer.info.game.GameController;
+import com.ack.adventureandconquer.info.game.adventure.Encounter;
+import com.ack.adventureandconquer.info.game.creature.npc.Npc;
+
+import java.util.List;
 
 
 public class EncounterDetailActivity extends ActionBarActivity {
@@ -27,11 +34,15 @@ public class EncounterDetailActivity extends ActionBarActivity {
 
         // Set top encounter text
         TextView textView = (TextView) findViewById(R.id.encounter_text);
-        System.out.println(GameController.getInstance().getLastEncounter());
-//        String encounterString = getIntent().getStringExtra("ddd");
-//        textView.setText(encounterString);
+        Encounter encounter = GameController.getInstance().getLastEncounter();
+        System.out.println(encounter);
 
-        // Set list view
+        StableArrayAdapter arrayAdapter = new StableArrayAdapter(this, encounter.getEncounterNpcs());
+        ListView npcList = (ListView) findViewById(R.id.npcList);
+        npcList.setAdapter(arrayAdapter);
+
+        textView.setText(encounter.getDescription());
+
     }
 
 
@@ -72,4 +83,46 @@ public class EncounterDetailActivity extends ActionBarActivity {
             return rootView;
         }
     }
+
+    private class StableArrayAdapter extends ArrayAdapter<Npc> {
+        private final Context context;
+        private List<Npc> npcList;
+
+        public StableArrayAdapter(Context context, List<Npc> objects) {
+            super(context, R.layout.npc_list_layout, objects);
+            this.context = context;
+            this.npcList = objects;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            Npc item = getItem(position);
+            return 0;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View rowView = inflater.inflate(R.layout.npc_list_layout, parent, false);
+            TextView unitName = (TextView) rowView.findViewById(R.id.unitName);
+            TextView armorClass = (TextView) rowView.findViewById(R.id.armorClass);
+            TextView hitDice = (TextView) rowView.findViewById(R.id.hitDice);
+            TextView hitPoints = (TextView) rowView.findViewById(R.id.hitPoints);
+
+            Npc npc = npcList.get(position);
+            unitName.setText("Type: " + npc.getClass().getSimpleName());
+            armorClass.setText("Armor: " + String.valueOf(npc.getArmorClass()));
+            hitDice.setText("Hit dice: " + String.valueOf(npc.getHitDice()));
+            hitPoints.setText("Hit points: " + String.valueOf(npc.getHitPoints()));
+
+            return rowView;
+        }
+    }
+
 }
