0c0222fdab28dbdac703778497335829455b61b4
Sascha <sascha.haeusler@netzbarkeit.ch>
2015-01-21 19:57:41 +0100

    Finished Blink Dog

--- app/src/main/java/com/ack/adventureandconquer/info/game/creature/npc/unusual/BlinkDog.java
+++ app/src/main/java/com/ack/adventureandconquer/info/game/creature/npc/unusual/BlinkDog.java
@@ -15,28 +15,39 @@ public class BlinkDog extends Npc {
     private static IsDice d6 = new D6();
 
     public static List<Npc> getPack() {
-        List<Npc> pack = new ArrayList<>();
         int packSize = d6.role();
-
-        for (int i = 1; i <= packSize; i++) {
-            pack.add(new BlinkDog());
-        }
-
-        return pack;
+        return createDogs(packSize);
     }
 
     public static List<Npc> getRoute() {
-        List<Npc> pack = new ArrayList<>();
         int packSize = d6.role() + d6.role();
-
-        for (int i = 1; i <= packSize; i++) {
-            pack.add(new BlinkDog());
-        }
-
-        return pack;
+        return createDogs(packSize);
     }
 
     public static List<Npc> getDen() {
         return getRoute();
+    }
+
+    private static List<Npc> createDogs(int packSize) {
+        List<Npc> pack = new ArrayList<>();
+
+        for (int i = 1; i <= packSize; i++) {
+            BlinkDog dog = new BlinkDog();
+            dog.roleHitPoints();
+
+            pack.add(dog);
+        }
+
+        return pack;
+    }
+
+    @Override
+    public int getDefaultArmorClass() {
+        return 4;
+    }
+
+    @Override
+    public int getDefaultHitDice() {
+        return 4;
     }
 }
