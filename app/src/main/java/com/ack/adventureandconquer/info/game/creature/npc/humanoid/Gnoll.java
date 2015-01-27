5fa59776cbc8e8f93ceb50e748400784f4908a0a
Sascha <sascha.haeusler@netzbarkeit.ch>
2015-01-27 17:10:14 +0100

    Added Gnoll

---
+++ app/src/main/java/com/ack/adventureandconquer/info/game/creature/npc/humanoid/Gnoll.java
@@ -0,0 +1,32 @@
+package com.ack.adventureandconquer.info.game.creature.npc.humanoid;
+
+/**
+ * Created by saskyrar on 27/01/15.
+ */
+public class Gnoll extends HumanoidMonster {
+    @Override
+    public String getExtraInformation() {
+
+        if (isChampion()) {
+            return "Champion\n+2 bonus to damage rolls from strength";
+        }
+        if (isSubChieftain()) {
+            return "Sub Chieftain\n+2 bonus to damage rolls";
+        }
+        if (isChieftain()) {
+            return "Chieftain\n+3 bonus to damage rolls";
+        }
+
+        return "+1 bonus to damage rolls due to their high strength";
+    }
+
+    @Override
+    public int getDefaultArmorClass() {
+        return 4;
+    }
+
+    @Override
+    public int getDefaultHitDice() {
+        return 2;
+    }
+}
--- app/src/main/java/com/ack/adventureandconquer/info/game/creature/npc/humanoid/GnollType.java
+++ app/src/main/java/com/ack/adventureandconquer/info/game/creature/npc/humanoid/GnollType.java
@@ -3,34 +3,172 @@ package com.ack.adventureandconquer.info.game.creature.npc.humanoid;
 import com.ack.adventureandconquer.info.game.creature.npc.IsNpcType;
 import com.ack.adventureandconquer.info.game.creature.npc.Npc;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by saskyrar on 17/01/15.
  */
-public class GnollType implements IsNpcType {
+public class GnollType extends HumanoidMonsterFactory<Gnoll> implements IsNpcType {
     @Override
     public boolean isLair(int number) {
-        return number >= 20;
+        return number <= 20;
+    }
+
+    @Override
+    protected int getGangSize() {
+        return d6.role();
+    }
+
+    @Override
+    protected int getWarBandSize() {
+        return d6.role();
+    }
+
+    @Override
+    protected int getVillageSize() {
+        return d10.role();
+    }
+
+    @Override
+    protected int getClericLevel() {
+        return d4.role() + 1;
+    }
+
+    @Override
+    protected int getMageLevel() {
+        return d4.role();
+    }
+
+    @Override
+    protected int getShamanChance() {
+        return 60;
+    }
+
+    @Override
+    protected int getWitchDoctorChance() {
+        return 50;
+    }
+
+    @Override
+    protected int getFemaleMonstersNumber(int maleNumbers) {
+        return maleNumbers / 2;
+    }
+
+    @Override
+    protected int getYoungMonstersNumber(int maleNumbers) {
+        return 0;
+    }
+
+    @Override
+    protected Gnoll createMonster() {
+        Gnoll gnoll = new Gnoll();
+        gnoll.roleHitPoints();
+
+        return gnoll;
+    }
+
+    @Override
+    protected Gnoll createChampion() {
+        Gnoll champion = new Gnoll();
+        champion.setChampion(true);
+
+        champion.setArmorClass(5);
+        champion.setHitDice(3);
+        champion.setHitPoints(16);
+
+        return champion;
+    }
+
+    @Override
+    protected Gnoll createSubChieftain() {
+        Gnoll subChieftain = new Gnoll();
+        subChieftain.setSubChieftain(true);
+
+        subChieftain.setArmorClass(6);
+        subChieftain.setHitDice(4);
+        subChieftain.setHitPoints(20);
+
+        return subChieftain;
+    }
+
+    @Override
+    protected Gnoll createChieftain() {
+        Gnoll chieftain = new Gnoll();
+        chieftain.setChieftain(true);
+
+        chieftain.setArmorClass(7);
+        chieftain.setHitDice(6);
+        chieftain.setHitPoints(28);
+
+        return chieftain;
+    }
+
+    @Override
+    protected Gnoll createFemale() {
+        Gnoll female = new Gnoll();
+        female.setType(HumanoidMonster.Type.FEMALE);
+
+        Orc orc = new Orc();
+        orc.roleHitPoints();
+
+        female.setArmorClass(orc.getArmorClass());
+        female.setHitDice(orc.getHitDice());
+        female.setAdditionalHitPoints(orc.getAdditionalHitPoints());
+        female.setHitPoints(orc.getHitPoints());
+
+        return female;
+    }
+
+    @Override
+    protected Gnoll createYoung() {
+        return null;
+    }
+
+    @Override
+    protected List<Npc> getAdditionalMonsters() {
+        List<Npc> monsters = new ArrayList<>();
+
+        boolean hasTrolls = d100.role() <= 33;
+        if (hasTrolls) {
+            int trollsNumber = d3.role();
+            for (int index = 0; index < trollsNumber; index++) {
+                Troll troll = new Troll();
+                troll.roleHitPoints();
+
+                monsters.add(troll);
+            }
+        }
+
+        boolean hasHyenas = d100.role() <= 66;
+        if (hasHyenas) {
+            int hyenasNumber = d4.role() + d4.role() + d4.role() + d4.role();
+            for (int index = 0; index < hyenasNumber; index++) {
+                // TODO: Find stats for hyenas
+//                Hye
+            }
+        }
+
+        return monsters;
     }
 
     @Override
     public List<Npc> getNormalWildnessEncounter() {
-        return null;
+        return getWarband();
     }
 
     @Override
     public String getNormalWildnessEncounterName() {
-        return null;
+        return "Warband";
     }
 
     @Override
     public List<Npc> getLairWildnessEncounter() {
-        return null;
+        return getVillage();
     }
 
     @Override
     public String getLairWildnessEncounterName() {
-        return null;
+        return "Village";
     }
 }
