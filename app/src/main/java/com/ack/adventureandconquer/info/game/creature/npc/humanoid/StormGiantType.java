e7cde74f680b642bd2aa56de29d7618312d44406
Sascha <sascha.haeusler@netzbarkeit.ch>
2015-01-30 08:08:51 +0100

    Added Storm Giant

--- app/src/main/java/com/ack/adventureandconquer/info/game/creature/npc/humanoid/StormGiantType.java
+++ app/src/main/java/com/ack/adventureandconquer/info/game/creature/npc/humanoid/StormGiantType.java
@@ -3,34 +3,63 @@ package com.ack.adventureandconquer.info.game.creature.npc.humanoid;
 import com.ack.adventureandconquer.info.game.creature.npc.IsNpcType;
 import com.ack.adventureandconquer.info.game.creature.npc.Npc;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by saskyrar on 17/01/15.
  */
-public class StormGiantType implements IsNpcType {
+public class StormGiantType extends HumanoidGiantFactory<StormGiant> implements IsNpcType {
     @Override
     public boolean isLair(int number) {
-        return number >= 45;
+        return number <= 45;
+    }
+
+    @Override
+    protected int getGangSize() {
+        return 1;
+    }
+
+    @Override
+    protected int getWarBandSize() {
+        return d3.role();
+    }
+
+    @Override
+    protected int getVillageSize() {
+        return d3.role();
+    }
+
+    @Override
+    protected StormGiant createMonster() {
+        StormGiant giant = new StormGiant();
+        giant.roleHitPoints();
+
+        return giant;
+    }
+
+    @Override
+    protected List<Npc> getAdditionalMonsters() {
+        return new ArrayList<>();
     }
 
     @Override
     public List<Npc> getNormalWildnessEncounter() {
-        return null;
+        return getWarband();
     }
 
     @Override
     public String getNormalWildnessEncounterName() {
-        return null;
+        return "Warband";
     }
 
     @Override
     public List<Npc> getLairWildnessEncounter() {
-        return null;
+        return getLair();
     }
 
     @Override
     public String getLairWildnessEncounterName() {
-        return null;
+        return "Lair";
     }
 }
