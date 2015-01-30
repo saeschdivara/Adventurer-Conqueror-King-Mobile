e871830b9ee0fa5d500db9a124e0d7221177270d
Sascha <sascha.haeusler@netzbarkeit.ch>
2015-01-30 07:46:57 +0100

    Added Frost Giant

---
+++ app/src/main/java/com/ack/adventureandconquer/info/game/creature/npc/humanoid/FrostGiant.java
@@ -0,0 +1,23 @@
+package com.ack.adventureandconquer.info.game.creature.npc.humanoid;
+
+import com.ack.adventureandconquer.info.game.creature.npc.Npc;
+
+/**
+ * Created by saskyrar on 30/01/15.
+ */
+public class FrostGiant extends Npc {
+    @Override
+    public String getExtraInformation() {
+        return "";
+    }
+
+    @Override
+    public int getDefaultArmorClass() {
+        return 5;
+    }
+
+    @Override
+    public int getDefaultHitDice() {
+        return 10;
+    }
+}
--- app/src/main/java/com/ack/adventureandconquer/info/game/creature/npc/humanoid/FrostGiantType.java
+++ app/src/main/java/com/ack/adventureandconquer/info/game/creature/npc/humanoid/FrostGiantType.java
@@ -2,35 +2,91 @@ package com.ack.adventureandconquer.info.game.creature.npc.humanoid;
 
 import com.ack.adventureandconquer.info.game.creature.npc.IsNpcType;
 import com.ack.adventureandconquer.info.game.creature.npc.Npc;
+import com.ack.adventureandconquer.info.game.creature.npc.animal.DireWolf;
+import com.ack.adventureandconquer.info.game.creature.npc.animal.PolarBear;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by saskyrar on 17/01/15.
  */
-public class FrostGiantType implements IsNpcType {
+public class FrostGiantType extends HumanoidGiantFactory<FrostGiant> implements IsNpcType {
     @Override
     public boolean isLair(int number) {
-        return number >= 30;
+        return number <= 30;
+    }
+
+    @Override
+    protected int getGangSize() {
+        return d2.role();
+    }
+
+    @Override
+    protected int getWarBandSize() {
+        return d4.role();
+    }
+
+    @Override
+    protected int getVillageSize() {
+        return d6.role();
+    }
+
+    @Override
+    protected FrostGiant createMonster() {
+        FrostGiant giant = new FrostGiant();
+        giant.roleHitPoints();
+
+        return giant;
+    }
+
+    @Override
+    protected List<Npc> getAdditionalMonsters() {
+
+        List<Npc> monsters = new ArrayList<>();
+
+        int animalDiceNumber = d10.role();
+        if (animalDiceNumber <= 8) {
+            int direWolfNumber = d6.role() + d6.role() + d6.role() + d6.role() + d6.role() + d6.role();
+
+            for (int index = 0; index < direWolfNumber; index++) {
+                DireWolf wolf = new DireWolf();
+                wolf.roleHitPoints();
+
+                monsters.add(wolf);
+            }
+        }
+        else {
+            int polarBearNumber = d6.role() + d6.role() + d6.role();
+
+            for (int index = 0; index < polarBearNumber; index++) {
+                PolarBear bear = new PolarBear();
+                bear.roleHitPoints();
+
+                monsters.add(bear);
+            }
+        }
+
+        return monsters;
     }
 
     @Override
     public List<Npc> getNormalWildnessEncounter() {
-        return null;
+        return getWarband();
     }
 
     @Override
     public String getNormalWildnessEncounterName() {
-        return null;
+        return "Warband";
     }
 
     @Override
     public List<Npc> getLairWildnessEncounter() {
-        return null;
+        return getLair();
     }
 
     @Override
     public String getLairWildnessEncounterName() {
-        return null;
+        return "Lair";
     }
 }
