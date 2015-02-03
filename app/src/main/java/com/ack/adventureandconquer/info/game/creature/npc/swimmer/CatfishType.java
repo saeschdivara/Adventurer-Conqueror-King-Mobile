d60479e642a5b0873af158e204faf9842ce7c693
Sascha <sascha.haeusler@netzbarkeit.ch>
2015-02-03 08:27:45 +0100

    Added Catfish

--- app/src/main/java/com/ack/adventureandconquer/info/game/creature/npc/swimmer/CatfishType.java
+++ app/src/main/java/com/ack/adventureandconquer/info/game/creature/npc/swimmer/CatfishType.java
@@ -2,26 +2,35 @@ package com.ack.adventureandconquer.info.game.creature.npc.swimmer;
 
 import com.ack.adventureandconquer.info.game.creature.npc.IsNpcType;
 import com.ack.adventureandconquer.info.game.creature.npc.Npc;
+import com.ack.adventureandconquer.info.game.creature.npc.NpcFactory;
 
 import java.util.List;
 
 /**
  * Created by saskyrar on 18/01/15.
  */
-public class CatfishType implements IsNpcType {
+public class CatfishType extends NpcFactory<Catfish> implements IsNpcType {
     @Override
     public boolean isLair(int number) {
         return false;
     }
 
     @Override
+    protected Catfish createMonster() {
+        Catfish monster = new Catfish();
+        monster.roleHitPoints();
+
+        return monster;
+    }
+
+    @Override
     public List<Npc> getNormalWildnessEncounter() {
-        return null;
+        return getMonsterList( d2.role() );
     }
 
     @Override
     public String getNormalWildnessEncounterName() {
-        return null;
+        return "School";
     }
 
     @Override
