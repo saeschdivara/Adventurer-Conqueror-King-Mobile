27d0c25691fb67a0e4b9860fe13b00f0bf94b0b8
Sascha <sascha.haeusler@netzbarkeit.ch>
2015-02-03 08:31:41 +0100

    Added Piranha

--- app/src/main/java/com/ack/adventureandconquer/info/game/creature/npc/swimmer/PiranhaFishType.java
+++ app/src/main/java/com/ack/adventureandconquer/info/game/creature/npc/swimmer/PiranhaFishType.java
@@ -2,26 +2,35 @@ package com.ack.adventureandconquer.info.game.creature.npc.swimmer;
 
 import com.ack.adventureandconquer.info.game.creature.npc.IsNpcType;
 import com.ack.adventureandconquer.info.game.creature.npc.Npc;
+import com.ack.adventureandconquer.info.game.creature.npc.NpcFactory;
 
 import java.util.List;
 
 /**
  * Created by saskyrar on 18/01/15.
  */
-public class PiranhaFishType implements IsNpcType {
+public class PiranhaFishType extends NpcFactory<PiranhaFish> implements IsNpcType {
     @Override
     public boolean isLair(int number) {
         return false;
     }
 
     @Override
+    protected PiranhaFish createMonster() {
+        PiranhaFish monster = new PiranhaFish();
+        monster.roleHitPoints();
+
+        return monster;
+    }
+
+    @Override
     public List<Npc> getNormalWildnessEncounter() {
-        return null;
+        return getMonsterList( d4.role() + d4.role() );
     }
 
     @Override
     public String getNormalWildnessEncounterName() {
-        return null;
+        return "Pack";
     }
 
     @Override
