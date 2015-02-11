54a14638a53950badee89dff97b6e57be395cf33
Sascha <sascha.haeusler@netzbarkeit.ch>
2015-02-11 09:04:39 +0100

    Added Sea Serpant

--- app/src/main/java/com/ack/adventureandconquer/info/game/creature/npc/swimmer/SeaSerpentType.java
+++ app/src/main/java/com/ack/adventureandconquer/info/game/creature/npc/swimmer/SeaSerpentType.java
@@ -2,26 +2,35 @@ package com.ack.adventureandconquer.info.game.creature.npc.swimmer;
 
 import com.ack.adventureandconquer.info.game.creature.npc.IsNpcType;
 import com.ack.adventureandconquer.info.game.creature.npc.Npc;
+import com.ack.adventureandconquer.info.game.creature.npc.NpcFactory;
 
 import java.util.List;
 
 /**
  * Created by saskyrar on 18/01/15.
  */
-public class SeaSerpentType implements IsNpcType {
+public class SeaSerpentType extends NpcFactory<SeaSerpent> implements IsNpcType {
     @Override
     public boolean isLair(int number) {
         return false;
     }
 
     @Override
+    protected SeaSerpent createMonster() {
+        SeaSerpent monster = new SeaSerpent();
+        monster.roleHitPoints();
+
+        return monster;
+    }
+
+    @Override
     public List<Npc> getNormalWildnessEncounter() {
-        return null;
+        return getMonsterList( d6.role() + d6.role() );
     }
 
     @Override
     public String getNormalWildnessEncounterName() {
-        return null;
+        return "Swarm";
     }
 
     @Override
