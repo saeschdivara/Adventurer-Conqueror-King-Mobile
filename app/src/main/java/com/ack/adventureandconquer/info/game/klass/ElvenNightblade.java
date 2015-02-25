948adb420bdd950c10b49c20bfff2b9e70bda26c
Sascha Häusler <sascha.haeusler@netzbarkeit.ch>
2015-02-25 08:42:34 +0100

    Starting with classes and all

---
+++ app/src/main/java/com/ack/adventureandconquer/info/game/klass/ElvenNightblade.java
@@ -0,0 +1,7 @@
+package com.ack.adventureandconquer.info.game.klass;
+
+/**
+ * Created by saskyrar on 25/02/15.
+ */
+public class ElvenNightblade implements IsClass {
+}
---
+++ app/src/main/java/com/ack/adventureandconquer/info/game/proficiency/Acrobatics.java
@@ -0,0 +1,30 @@
+package com.ack.adventureandconquer.info.game.proficiency;
+
+import com.ack.adventureandconquer.info.game.Proficiency;
+
+/**
+ * Created by saskyrar on 25/02/15.
+ */
+public class Acrobatics extends Proficiency {
+
+    @Override
+    public String getName() {
+        return ACROBATICS;
+    }
+
+    @Override
+    public String getDescription() {
+        return "The character is trained to jump, tumble, somersault, and free-run around obstacles. " +
+                "The character gains +2 to saving throws where agility would help avoid the situation, " +
+                "such as tilting floors and pit traps. In lieu of moving during a round, the " +
+                "character may attempt a proficiency throw of 20+ to tumble behind an opponent in " +
+                "melee. The proficiency throw required for the tumble is reduced by 1 per level " +
+                "of experience the character possesses. If successful, the character is behind his " +
+                "opponent. The opponent can now be attacked with a +2 bonus to the attack throw, " +
+                "and gains no benefit from his shield. Thieves and others eligible to backstab " +
+                "an opponent gain their usual +4 on the attack throw and bonus to damage. " +
+                "Characters with an encumbrance of 6 stone or more may not tumble. Note " +
+                "that elven nightblades automatically begin play with this ability as part of " +
+                "their class.";
+    }
+}
