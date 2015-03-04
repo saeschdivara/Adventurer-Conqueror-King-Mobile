bdb4161b41c409c1b171235ab51a3ae7409bb7a9
Sascha Häusler <sascha.haeusler@netzbarkeit.ch>
2015-03-04 07:30:13 +0100

    Implemented Anti Paladin

--- app/src/main/java/com/ack/adventureandconquer/info/game/klass/AntiPaladin.java
+++ app/src/main/java/com/ack/adventureandconquer/info/game/klass/AntiPaladin.java
@@ -3,6 +3,54 @@ package com.ack.adventureandconquer.info.game.klass;
 /**
  * Created by saskyrar on 03/03/15.
  */
-public class AntiPaladin implements IsClass {
+public class AntiPaladin extends CharacterClass {
+    @Override
+    public String getTitle() {
+
+        long experience = getExperience();
+
+        if (experience == 0) {
+            return "Miscreant";
+        }
+        else if (experience >= 1850 && experience < 3700) {
+            return "Despoiler";
+        }
+        else if (experience >= 3700 && experience < 7400) {
+            return "Avenger";
+        }
+        else if (experience >= 7400 && experience < 14800) {
+            return "Defiler";
+        }
+        else if (experience >= 14800 && experience < 29600) {
+            return "Blackguard";
+        }
+        else if (experience >= 29600 && experience < 60000) {
+            return "Destroyer";
+        }
+        else if (experience >= 60000 && experience < 120000) {
+            return "Warduke";
+        }
+        else if (experience >= 120000 && experience < 240000) {
+            return "Malefactor";
+        }
+        else if (experience >= 240000 && experience < 360000) {
+            return "Malefactor Lord";
+        }
+        else if (experience >= 360000 && experience < 480000) {
+            return "Malefactor Lord (X)";
+        }
+        else if (experience >= 480000 && experience < 600000) {
+            return "Malefactor Lord (XI)";
+        }
+        else if (experience >= 600000 && experience < 720000) {
+            return "Malefactor Lord (XII)";
+        }
+        else if (experience >= 720000 && experience < 840000) {
+            return "Malefactor Lord (XIII)";
+        }
+        else {
+            return "Dark Lord of Malefaction";
+        }
+    }
 }
 
--- app/src/main/java/com/ack/adventureandconquer/info/game/klass/Cleric.java
+++ app/src/main/java/com/ack/adventureandconquer/info/game/klass/Cleric.java
@@ -18,40 +18,40 @@ public class Cleric extends CharacterClass {
         if (experience == 0) {
             return "Catechist";
         }
-        else if (experience >= 1500 && experience <= 3000) {
+        else if (experience >= 1500 && experience < 3000) {
             return "Acolyte";
         }
-        else if (experience >= 3000 && experience <= 6000) {
+        else if (experience >= 3000 && experience < 6000) {
             return "Priest";
         }
-        else if (experience >= 6000 && experience <= 12000) {
+        else if (experience >= 6000 && experience < 12000) {
             return "Curate";
         }
-        else if (experience >= 12000 && experience <= 24000) {
+        else if (experience >= 12000 && experience < 24000) {
             return "Vicar";
         }
-        else if (experience >= 24000 && experience <= 50000) {
+        else if (experience >= 24000 && experience < 50000) {
             return "Rector";
         }
-        else if (experience >= 50000 && experience <= 100000) {
+        else if (experience >= 50000 && experience < 100000) {
             return "Prelate";
         }
-        else if (experience >= 100000 && experience <= 200000) {
+        else if (experience >= 100000 && experience < 200000) {
             return "Bishop";
         }
-        else if (experience >= 200000 && experience <= 300000) {
+        else if (experience >= 200000 && experience < 300000) {
             return "Patriarch";
         }
-        else if (experience >= 300000 && experience <= 400000) {
+        else if (experience >= 300000 && experience < 400000) {
             return "Patriarch (X)";
         }
-        else if (experience >= 400000 && experience <= 500000) {
+        else if (experience >= 400000 && experience < 500000) {
             return "Patriarch (XI)";
         }
-        else if (experience >= 500000 && experience <= 600000) {
+        else if (experience >= 500000 && experience < 600000) {
             return "Patriarch (XII)";
         }
-        else if (experience >= 600000 && experience <= 700000) {
+        else if (experience >= 600000 && experience < 700000) {
             return "Patriarch (XIII)";
         }
         else {
