cc7e0b9a3967110330c19f9bf4e70baa73437ec5
Sascha Häusler <sascha.haeusler@netzbarkeit.ch>
2015-03-10 08:40:35 +0100

    Now there is a helper class for images

--- app/src/main/java/com/ack/adventureandconquer/ui/MainActivity.java
+++ app/src/main/java/com/ack/adventureandconquer/ui/MainActivity.java
@@ -2,9 +2,6 @@ package com.ack.adventureandconquer.ui;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -38,6 +35,7 @@ import com.ack.adventureandconquer.info.game.adventure.Swamp;
 import com.ack.adventureandconquer.info.game.adventure.Wilderness;
 import com.ack.adventureandconquer.info.game.adventure.Woods;
 import com.ack.adventureandconquer.ui.character.LoginActivity;
+import com.ack.adventureandconquer.ui.helper.image.ImageSizeHelper;
 
 
 public class MainActivity extends ActionBarActivity
@@ -53,44 +51,6 @@ public class MainActivity extends ActionBarActivity
      */
     private CharSequence mTitle;
 
-    public static int calculateInSampleSize(
-            BitmapFactory.Options options, int reqWidth, int reqHeight) {
-        // Raw height and width of image
-        final int height = options.outHeight;
-        final int width = options.outWidth;
-        int inSampleSize = 1;
-
-        if (height > reqHeight || width > reqWidth) {
-
-            final int halfHeight = height / 2;
-            final int halfWidth = width / 2;
-
-            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
-            // height and width larger than the requested height and width.
-            while ((halfHeight / inSampleSize) > reqHeight
-                    && (halfWidth / inSampleSize) > reqWidth) {
-                inSampleSize *= 2;
-            }
-        }
-
-        return inSampleSize;
-    }
-
-    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
-                                                         int reqWidth, int reqHeight) {
-
-        // First decode with inJustDecodeBounds=true to check dimensions
-        final BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        BitmapFactory.decodeResource(res, resId, options);
-
-        // Calculate inSampleSize
-        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
-
-        // Decode bitmap with inSampleSize set
-        options.inJustDecodeBounds = false;
-        return BitmapFactory.decodeResource(res, resId, options);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -256,7 +216,7 @@ public class MainActivity extends ActionBarActivity
 
         ImageView clearing_button = (ImageView) findViewById(imageViewId);
         clearing_button.setImageBitmap(
-                decodeSampledBitmapFromResource(getResources(), imageResourceId, height, width)
+            ImageSizeHelper.decodeSampledBitmapFromResource(getResources(), imageResourceId, height, width)
         );
     }
 
